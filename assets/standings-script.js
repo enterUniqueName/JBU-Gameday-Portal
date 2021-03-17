24e4c35e0f2e42b268f2466345651b182d1fdc68
enterUniqueName <53091774+enterUniqueName@users.noreply.github.com>
2021-03-17 16:57:46 -0400

    Uploaded files

---
+++ assets/standings-script.js
@@ -0,0 +1,57 @@
+var obj = {
+    "start": 0,
+    "count": 10,
+    "name": "standings-html-ad-f66e18",
+    "sport_id": null,
+    "count_breakpoints": null,
+    "css_class": "",
+    "pinned_id": "",
+    "extra": {},
+    "context": null,
+    "type": "ads",
+    "id": "861fbe8f-9eef-42fc-a98b-df0de0f41934",
+    "data": {
+        "location": {
+            "id": 58,
+            "title": "Standings HTML Ad",
+            "type": "html",
+            "effect": "none",
+            "fixed_image_sizes": false,
+            "excluded_sport_ids": [],
+            "slick": false,
+            "dfp": false,
+            "sidearm_dfp": false,
+            "common_page": false,
+            "html_template": null,
+            "name": "standings-html-ad-f66e18",
+            "autoplay_speed": 8,
+            "accessibility_icons": false,
+            "sizes": null,
+            "slick_options": null,
+            "dfp_sizes": null
+        },
+        "campaigns": [{
+            "id": 1041,
+            "title": "Standings FBL Ad",
+            "start_date": "2017-06-28T00:00:00",
+            "end_date": null,
+            "open_in_new_window": true,
+            "link": "#",
+            "icon": null,
+            "html": "&lt;div id=&quot;standings&quot;&gt;\r\n&lt;div class=&quot;sectionTitle&quot;&gt;\r\n&lt;div class=&quot;title&quot;&gt;Standings&lt;/div&gt;\r\n&lt;/div&gt;\r\n\r\n&lt;div id=&quot;standingsTable&quot;&gt;&lt;/div&gt;\r\n&lt;/div&gt;\r\n&lt;script&gt;$(&quot;#standingsTable&quot;).load(&quot;https://static.mgoblue.com/custompages/library/standings/football-standings.html&quot;)&lt;/script&gt;",
+            "picture": null,
+            "css_class": null,
+            "is_dummy": false,
+            "hover_text": null
+        }],
+        "content_id": "standings-html-ad-f66e18",
+        "content_title": "standings-html-ad-f66e18",
+        "content_date": null,
+        "content_url": null,
+        "content_image_url": null
+    },
+    "mode": "web"
+};
+if (!("sidearmComponents" in window))
+    window.sidearmComponents = [];
+window.sidearmComponents.push(obj);
